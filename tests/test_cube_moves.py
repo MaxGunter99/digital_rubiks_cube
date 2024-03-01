e68c55936969cac0005228d3af62ab58a0530fc0
Max Gunter <maxgunter99@gmail.com>
2024-03-01 16:48:56 -0600

    all moves implemented and tests passing

--- tests/test_cube_moves.py
+++ tests/test_cube_moves.py
@@ -132,13 +132,13 @@ class TestMoves( unittest.TestCase ):
         test_data_path = "tests/test_cases/middle_horizontal_right_1.json" 
         self.run_test_file( test_data_path )
 
-    # def test__move_cube__bottom_horizontal_left_1( self ):
-    #     test_data_path = "tests/test_cases/bottom_horizontal_left_1.json" 
-    #     self.run_test_file( test_data_path )
+    def test__move_cube__bottom_horizontal_left_1( self ):
+        test_data_path = "tests/test_cases/bottom_horizontal_left_1.json" 
+        self.run_test_file( test_data_path )
 
-    # def test__move_cube__bottom_horizontal_right_1( self ):
-    #     test_data_path = "tests/test_cases/bottom_horizontal_right_1.json" 
-    #     self.run_test_file( test_data_path )
+    def test__move_cube__bottom_horizontal_right_1( self ):
+        test_data_path = "tests/test_cases/bottom_horizontal_right_1.json" 
+        self.run_test_file( test_data_path )
 
     def test__move_cube__left_vertical_up_1( self ):
         test_data_path = "tests/test_cases/left_vertical_up_1.json" 
@@ -188,6 +188,14 @@ class TestMoves( unittest.TestCase ):
 
     def test__custom__move_cube__top_horizontal_left_1( self ):
         test_data_path = "tests/test_cases/custom_top_horizontal_left_1.json" 
+        self.run_test_file( test_data_path )
+
+    def test__custom__move_cube__bottom_horizontal_right_1( self ):
+        test_data_path = "tests/test_cases/custom_bottom_horizontal_right_1.json" 
+        self.run_test_file( test_data_path )
+
+    def test__custom__move_cube__bottom_horizontal_left_1( self ):
+        test_data_path = "tests/test_cases/custom_bottom_horizontal_left_1.json" 
         self.run_test_file( test_data_path )
 
 
