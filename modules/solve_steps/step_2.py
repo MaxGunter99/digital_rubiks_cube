a9de7d29e026ec0ae861fa9dd2e10ea411a04f38
Max Gunter <maxgunter99@gmail.com>
2024-04-12 15:24:51 -0500

    adding refresh data function to find corner pieces

--- modules/solve_steps/step_2.py
+++ modules/solve_steps/step_2.py
@@ -34,6 +34,41 @@ def solve_cube__step_2( cube_client, test_id=None ):
 	game_loop_iteration = 0
 	game_loop_complete = False
 
+	# we need to find the top corner pieces to check if they are perfect
+	def refresh_data():
+
+		if LOG_STEP_INFO == True:
+			print( "REFRESHING DATA" )
+		top_indexes_to_fix = {
+            # row, index: is_perfect
+            ( 0, 0 ): False,
+            ( 0, 2 ): False,
+            ( 2, 0 ): False,
+            ( 2, 2 ): False
+        }
+		top_color_value = cube_client.top_side[1][1]
+		top_color_locations = cube_client.check_sides( top_color_value )
+		top_row_pieces = []
+		bottom_row_pieces = []
+
+		for side_name, side_data in top_color_locations.items():
+			brick_data = side_data.get( "brick_data", [] )
+			parent_locations = []
+
+			for brick in brick_data:
+				parent_data = brick.get( "parent_data" )
+				parent_color = parent_data.get( "parent_value" )
+				parent_row_index = parent_data.get( "parent_row_index" )
+				if parent_color == top_color_value and len( brick.keys() ) == 3:
+
+					if side_name == "top_side" or side_name in [ "front_side", "left_side", "right_side", "back_side" ] and parent_row_index == 0:
+						top_row_pieces.append( brick )
+
+					elif side_name == "bottom_side" or side_name in [ "front_side", "left_side", "right_side", "back_side" ] and parent_row_index == 2:
+						bottom_row_pieces.append( brick )
+
+		return top_row_pieces, bottom_row_pieces
+
 	while (
         game_loop_max_count < 10 
         or game_loop_complete == False and game_loop_iteration < game_loop_max_count
@@ -46,8 +81,14 @@ def solve_cube__step_2( cube_client, test_id=None ):
 		if game_loop_iteration >= game_loop_max_count:
 			break
 
+		top_row_pieces, bottom_row_pieces = refresh_data()
+		if 3 >= len( top_row_pieces + bottom_row_pieces ) > 4:
+			raise Exception( f"Error in step 2: refresh_data function did not not find corner pieces, returned {len( top_row_pieces + bottom_row_pieces )} but should be 4" )
+		
+		print( f"top_row_pieces: {top_row_pieces}" )
+		print( f"bottom_row_pieces: {bottom_row_pieces}" )
 
-		# ... step logic
+		print( f"top_row_pieces: {len( top_row_pieces + bottom_row_pieces )}" )
 		raise Exception( "STEP NOT IMPLEMENTED" )
 
 	if len( step_errors ):
